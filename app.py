655b9fbc80bb8593af91d3173850f1f88c741541
nfestasoftware <nfestasoftware@gmail.com>
2023-10-22 16:37:08 +0000

    rectified code on line 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print('apps are ready for our fir deployment')
-print('please, let's start deployment1')
+print('please, let start deployment1')
 print('please, we are starting deployment2')
 print('please, testing rebase from merge')
 print('please, adding second line for testing rebase from merge')
